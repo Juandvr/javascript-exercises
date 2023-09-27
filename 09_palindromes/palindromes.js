915e590629ab4fcfb487b0385c5ec4b70cdc50ae
Juandvr <villabonajuan203@gmail.com>
2023-09-26 22:05:49 -0500

    Exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let join = string.replaceAll(",", "").replaceAll(" ", "").replaceAll("!", "").replaceAll(".", "");
+    let rev = join.split("").reverse().join("");
+    //console.log(join);
+    //console.log(rev);
+    if (rev.toLowerCase() == join.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+//console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
