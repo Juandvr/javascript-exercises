af5b7710e4213cb48b69646df7cc30a0e52489a1
Juandvr <villabonajuan203@gmail.com>
2023-09-29 19:04:10 -0500

    exercise completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let list = [];
+    for(let i = 0; i < arr.length; i++){
+        list.push(arr[i].title);
+    }
+    return list;
 };
 
 // Do not edit below this line
